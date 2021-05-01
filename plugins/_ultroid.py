30f9d953ca9a4b490216a554be5c9ecffed2d608
RJKannadaAdmin <83166159+RJKannadaAdmin@users.noreply.github.com>
2021-05-01 19:09:25 +0530

    Update _ultroid.py

--- plugins/_ultroid.py
+++ plugins/_ultroid.py
@@ -11,9 +11,9 @@ from telethon.errors import ChatSendInlineForbiddenError
 from . import *
 
 REPOMSG = (
-    "• **ULTROID USERBOT** •\n\n",
-    "• Repo - [Click Here](https://github.com/TeamUltroid/Ultroid)\n",
-    "• Support - @UltroidSupport",
+    "• **Super USERBOT** •\n\n",
+    "• Repo - Click Here\n",
+    "• Support - @TheHacker90",
 )
 
 
